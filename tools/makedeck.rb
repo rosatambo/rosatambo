54121b804c6cfd92d51be59281958c45aa80de05
Rosatambo <none@specified.com>
2014-08-01 15:27:39 -0400

    Update lastgen to 28

--- tools/makedeck.rb
+++ tools/makedeck.rb
@@ -4,14 +4,12 @@ targetunused = 10
 
 nuf 		= "/home/bitcoin/rosa/decks/nowusing.txt"
 nowusingfile 	= File.open(nuf, "r")
-  nowusing   	= nowusingfile.readline.chomp
-  nowusing 	= nowusing.to_i
+  nowusing   	= nowusingfile.readline.chomp.to_i
 nowusingfile.close
 
 lgf 		= "/home/bitcoin/rosa/decks/lastgen.txt" 
 lastgenfile 	= File.open(lgf, "r")
-  lastgen	= lastgenfile.readline.chomp
-  lastgen 	= lastgen.to_i
+  lastgen	= lastgenfile.readline.chomp.to_i
 lastgenfile.close
 
 tobuild = targetunused - ( lastgen - nowusing )
@@ -28,34 +26,32 @@ tobuild.times do
   raw = ""
 
   rolls.times do
-    raw = raw + "R,P,S,"    #start with even number of R,P,S so that the deck is mostly balanced
-    @rockrand = rand(1..50)  #randomize number of rocks in deck
-    @rockrand.times do
-      raw = raw + "R,"
-    end
-    @paperrand = rand(1..50)  #randomize number of scisors in deck
-    @paperrand.times do
-      raw = raw + "P,"
-    end
-    @scisrand = rand(1..50)  #randomize number of scissors in deck
-    @scisrand.times do
-      raw = raw + "S,"
-    end
+    raw = raw + "RPS" 	#start with equal number of R,P,S so that the deck is mostly balanced
+  end
+  @rockrand = rand(1..50)  	#randomize number of rocks in deck
+  @rockrand.times do
+    raw += "R"
+  end
+  @paperrand = rand(1..50)  	#randomize number of scisors in deck
+  @paperrand.times do
+    raw += "P"
+  end
+  @scisrand = rand(1..50)  	#randomize number of scissors in deck
+  @scisrand.times do
+    raw += "S"
   end
 
   rolls = rolls *3 + @rockrand + @paperrand + @scisrand
 
   raw = raw[0...-1]
-  throws = raw.split(",").shuffle!.shuffle!.shuffle!
+  throws = raw.split("").shuffle!.shuffle!.shuffle!
   deck = throws.join
   deckhash = Digest::SHA256.hexdigest(deck)
 
-
   lastgen += 1
-  fname = "/home/bitcoin/rosa/decks/" +lastgen.to_s + ".txt"
-  hname = "/home/bitcoin/rosa/hashes/" +lastgen.to_s + ".hash"
-
-#  puts "Building #{fname}"
+  fname = "/home/bitcoin/rosa/decks/#{lastgen}.txt"
+  hname = "/home/bitcoin/rosa/hashes/#{lastgen}.hash"
+  phname = "/home/bitcoin/rosa/decks-used/#{lastgen}.txt"
 
   deckfile = File.open(fname, "w")
   deckfile.print "Rosatambo Throw Deck # #{lastgen} \n"
@@ -68,10 +64,15 @@ tobuild.times do
   hashfile.print "Rosatambo Hash File for Deck # #{lastgen} \n"
   hashfile.puts deckhash
   hashfile.close
+
+  placeholderfile = File.open(phname, "w")
+  placeholderfile.print "This file has not yet been released. Please check back in 24 hours.\n"
+  placeholderfile.puts deckhash
+  placeholderfile.close
+
 end
 
 commitcomment = "Update lastgen to #{lastgen}"
-#puts commitcomment
 lastgenfile 	= File.open(lgf, "w")
 lastgenfile.print lastgen
 lastgenfile.close
